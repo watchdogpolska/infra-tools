6254096f4b6bb38032b3ebe4fe65c33e8438dcc1
Krzysztof Biernat <krzysztof@biernat.in>
2018-07-20 11:30:35 +0200

    add maxokdelay parameter

--- scripts/run_locked.sh
+++ scripts/run_locked.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 if [ -z "$2" ]; then
-  echo Usage: $0 lock_name command...
+  echo Usage: $0 lock_name [max_ok_delay] command...
   exit 1
 fi
 
 LOCKDIR=/var/lock
-LOCKFILE=${LOCKDIR}/$1
+LOCKFILE=${LOCKDIR}/$1.lock
+TIMEFILE=${LOCKDIR}/$1.time
 shift
 
 exec 99>$LOCKFILE
-flock -xn 99 || { echo Lock acquired by other process $(( $(date +%s)-$(cat $LOCKFILE-time) )) seconds ago, exiting && exit 1; }
-date +%s > $LOCKFILE-time
-
+flock -xn 99 || { echo Lock acquired by other process $(( $(date +%s)-$(cat $TIMEFILE) )) seconds ago, exiting && exit 1; }
+date +%s > $TIMEFILE
+[[ $1 =~ ^[0-9]*$ ]] && { echo $1 > $TIMEFILE.maxokdelay; shift; }
 trap "flock -u 99" EXIT
 
 eval $@
+exit $?
 
